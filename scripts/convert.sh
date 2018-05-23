0ae90578575b0f1379bc2fdb18797f47b84fd0f7
Christoph Wolfes <christoph.wolfes@triology.de>
2018-05-23 16:15:54 +0200

    adds some documentation to the scripts

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -4,7 +4,7 @@ document_name="document.pdf"
 else
 document_name="$1"
 fi
-# search and replace ß because
+# search and replace ß because it fails with latex T1 encoding leads to SS
 find . -type f -name "*.md" -print0 | xargs -0 sed -i 's/ß/\\"s/g'
 /usr/local/bin/pandoc.sh $document_name
 find . -type f -name "*.md" -print0 | xargs -0 sed -i 's/\\"s/ß/g'
--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-
+#use --verbose to enable debugging output
 pandoc -o $1 --template=/usr/local/pandoc/template/template.tex \
 --include-in-header=/usr/local/pandoc/template/preamble.tex --listings \
 --highlight-style=pygments --variable=fontsize:12pt \
