094a645a3e48b445a9fe00a105dae95b32ad7dd7
Christoph Wolfes <christoph.wolfes@triology.de>
2021-01-12 13:59:34 +0100

    updates for itz layout

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -4,10 +4,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 
 #use --verbose to enable debugging output
+
+
 pandoc \
   --output=document.pdf \
-  --template=/usr/local/pandoc/template/template.tex \
-  --include-in-header=/usr/local/pandoc/template/preamble.tex \
+  --template=/usr/local/pandoc/template/"${1:-cloudogu}"/template.tex \
+  --include-in-header=/usr/local/pandoc/template/"${1:-cloudogu}"/preamble.tex \
   --listings \
   --highlight-style=pygments \
   --variable=fontsize:12pt \
@@ -16,7 +18,10 @@ pandoc \
   --toc \
   --filter=pandoc-plantuml-filter \
   --latex-engine=xelatex \
-  "$@"
+  --verbose \
+  "$2"
+
+ls /usr/local/pandoc/template/itz-bund
 
 if [ $? != 0 ]; then
     echo -e "${RED}Error: Could not convert files"
